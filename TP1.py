9735605431656099350d980fcffae1b4776ab177
Sacha <sacha@MacBook-Pro-de-Sacha.local>
2020-04-03 11:22:26 +0200

    Initial commit

---
+++ TP1.py
@@ -0,0 +1,30 @@
+# i = 0
+# while i <= 20 and i >= 0:
+#     if(i%2 != 0):
+#
+#         print('Le résultat est', i)
+#         i= i+1
+#     else:
+#         i= i+1
+
+#for i in range(21):
+#   if(i%2 == 0):
+#
+#        print('Le résultat est', i)
+#        i= i+1
+#    else:
+#        i= i+1
+
+# Exercice 3
+# n=int(input("Saisissez une valeur positive : "))
+# s=0
+# for x in range (n):
+#     if(x%2 != 0):
+#         print("impair", x)
+#         s=s+x
+#     else:
+#         print("faux")
+# print("La somme des impairs est", s)
+
+#Exercice 4
+
